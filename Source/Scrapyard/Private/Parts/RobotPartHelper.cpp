50ff35152751aea2d8ea91fa5e24d582d30fac43
dk <davidkasofsky@gmail.com>
2018-09-25 19:27:18 -0400

    ok

--- Source/Scrapyard/Private/Parts/RobotPartHelper.cpp
+++ Source/Scrapyard/Private/Parts/RobotPartHelper.cpp
@@ -5,6 +5,7 @@
 #include "AssetRegistryModule.h"
 #include "EngineUtils.h"
 
+// these are useless and wrong now but hey
 const FString URobotPartHelper::HeadPath = FString(TEXT("/Game/Parts/Head"));
 const FString URobotPartHelper::CorePath = FString(TEXT("/Game/Parts/Core"));
 const FString URobotPartHelper::ArmsPath = FString(TEXT("/Game/Parts/Arms"));
@@ -13,51 +14,51 @@ const FString URobotPartHelper::LegsPath = FString(TEXT("/Game/Parts/Legs"));
 template <typename T>
 TArray<T*> URobotPartHelper::GetAllPartsInFolder(FString Path)
 {
-	TArray<UObject*> Assets;
-//	EngineUtils::FindOrLoadAssetsByPath(TEXT("/Game/Parts/Head"), Assets, EngineUtils::ATL_Regular);
-	
-	UE_LOG(LogTemp, Warning, TEXT("path is: %s"), *Path);
-	bool Result = EngineUtils::FindOrLoadAssetsByPath(Path, Assets, EngineUtils::ATL_Regular);
-	if (Result) {
-		UE_LOG(LogTemp, Warning, TEXT("got this many assets: %i"), Assets.Num());
-	}
+  TArray<UObject*> Assets;
+//  EngineUtils::FindOrLoadAssetsByPath(TEXT("/Game/Parts/Head"), Assets, EngineUtils::ATL_Regular);
+  
+  UE_LOG(LogTemp, Warning, TEXT("path is: %s"), *Path);
+  bool Result = EngineUtils::FindOrLoadAssetsByPath(Path, Assets, EngineUtils::ATL_Regular);
+  if (Result) {
+    UE_LOG(LogTemp, Warning, TEXT("got this many assets: %i"), Assets.Num());
+  }
 
-	TArray<T*> Parts;
-	for (UObject* Asset : Assets)
-	{
-		T* Part = Cast<T>(Asset);
-		if (Part)
-		{
-			Parts.Add(Part);
-		}
-		else
-		{
-			// probably abort cause something's wrong
-		}
-	}
-	UE_LOG(LogTemp, Warning, TEXT("got this many parts: %i"), Parts.Num());
+  TArray<T*> Parts;
+  for (UObject* Asset : Assets)
+  {
+    T* Part = Cast<T>(Asset);
+    if (Part)
+    {
+      Parts.Add(Part);
+    }
+    else
+    {
+      // probably abort cause something's wrong
+    }
+  }
+  UE_LOG(LogTemp, Warning, TEXT("got this many parts: %i"), Parts.Num());
 
-	return Parts;
+  return Parts;
 }
 
 TArray<UHeadPart*> URobotPartHelper::GetAllHeads()
 {
-	return URobotPartHelper::GetAllPartsInFolder<UHeadPart>(HeadPath);
+  return URobotPartHelper::GetAllPartsInFolder<UHeadPart>(HeadPath);
 }
 
 TArray<UCorePart*> URobotPartHelper::GetAllCores()
 {
-	return URobotPartHelper::GetAllPartsInFolder<UCorePart>(CorePath);
+  return URobotPartHelper::GetAllPartsInFolder<UCorePart>(CorePath);
 }
 
 TArray<UArmsPart*> URobotPartHelper::GetAllArms()
 {
-	return URobotPartHelper::GetAllPartsInFolder<UArmsPart>(ArmsPath);
+  return URobotPartHelper::GetAllPartsInFolder<UArmsPart>(ArmsPath);
 }
 
 TArray<ULegsPart*> URobotPartHelper::GetAllLegs()
 {
-	return URobotPartHelper::GetAllPartsInFolder<ULegsPart>(LegsPath);
+  return URobotPartHelper::GetAllPartsInFolder<ULegsPart>(LegsPath);
 }
 
 ARobotPartCardActor* URobotPartHelper::SpawnRobotPartCardActor(UWorld* World, TSubclassOf<URobotPart> RobotPart, FVector Loc, FRotator Rot, FActorSpawnParameters SpawnParams)
